93f4e758b102d0c6f312366bd82770ec4b7fca2d
masakiishii <masaki.ishii511@gmail.com>
2014-12-17 19:36:35 +0900

    add DTDLine.java

---
+++ src/DTDLine.java
@@ -0,0 +1,23 @@
+package org.peg4d.data;
+
+import java.util.Map;
+
+
+public class DTDLine {
+	private String                 column        = null;
+	private Map<String, DTDObject> dtdobjectmap  = null;
+	
+	public DTDLine(String column, Map<String, DTDObject> dtdobjectmap) {
+		this.column       = column;
+		this.dtdobjectmap = dtdobjectmap;
+	}
+	
+	public String getColumn() {
+		return this.column;
+	}
+
+	public Map<String, DTDObject> getDTDObjectMap() {
+		return this.dtdobjectmap;
+	}
+
+}
